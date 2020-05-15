bc1d970c505af773968fb99d6bdf52ffb863ddda
Snehalata <snehlata.paliwal@gmail.com>
2020-05-15 16:07:50 +0530

    save data on local DB

--- PaktolusSolutionsTest/ViewController.swift
+++ PaktolusSolutionsTest/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+
 
 
 class ViewController: UIViewController {
@@ -15,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var age: UITextField!
     @IBOutlet weak var weight: UITextField!
     @IBOutlet weak var height: UITextField!
+    var calories = 0.0
     let delegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
@@ -24,9 +27,33 @@ class ViewController: UIViewController {
     
     @IBAction func calculateCalories(sender: UIButton){
         let velocity = LocationManager.SharedManager.distance ?? 0.0 / Double(delegate.timeInterval)
-        let calories = (0.035 * Double((weight.text! as NSString).doubleValue)) + ((Double((Int(velocity) ^ 2)) / Double((height.text! as NSString).doubleValue))) * (0.029) * (Double((weight.text! as NSString).doubleValue))
-        print(calories)
-        
+         calories = (0.035 * Double((weight.text! as NSString).doubleValue)) + ((Double((Int(velocity) ^ 2)) / Double((height.text! as NSString).doubleValue))) * (0.029) * (Double((weight.text! as NSString).doubleValue))
+        saveDataOnLocalDB()
+    }
+    
+    func saveDataOnLocalDB() {
+      guard let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate else {
+        return
+      }
+      let managedContext =
+        appDelegate.persistentContainer.viewContext
+      let entity =
+        NSEntityDescription.entity(forEntityName: "UserInfo",
+                                   in: managedContext)!
+      let person = NSManagedObject(entity: entity,
+                                   insertInto: managedContext)
+        person.setValue(name.text, forKeyPath: "userName")
+        person.setValue(age.text, forKeyPath: "userAge")
+        person.setValue(height.text, forKeyPath: "userHeight")
+        person.setValue(weight.text, forKeyPath: "userWeight")
+        person.setValue(calories, forKeyPath: "caloriesData")
+
+      do {
+        try managedContext.save()
+      } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+      }
     }
 
 }
